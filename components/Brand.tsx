bf776436cbd3b748bde792d5f7c16a0f92bb9c8b
jorluisproyect <jorluisproyect@gmail.com>
2026-09-19 16:10:59 -0400

    Make TURNAVIA logo return to home

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -1,5 +1,6 @@
+import Link from "next/link";
 import { Route } from "lucide-react";
 
 export function Brand() {
-  return <div className="brand"><span className="logo"><Route size={19}/></span><span>Turnavia</span></div>;
+  return <Link href="/" className="brand" aria-label="Ir al inicio de TURNAVIA"><span className="logo"><Route size={19}/></span><span>Turnavia</span></Link>;
 }
