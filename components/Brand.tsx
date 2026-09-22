ba5cdaafd3af8c501a4dcb6f01ae67643236d33b
jorluisproyect <jorluisproyect@gmail.com>
2026-09-21 20:44:21 -0400

    Rebrand Turnavia to TUCITA

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -2,5 +2,5 @@ import Link from "next/link";
 import { Route } from "lucide-react";
 
 export function Brand() {
-  return <Link href="/" className="brand" aria-label="Ir al inicio de TURNAVIA"><span className="logo"><Route size={19}/></span><span>Turnavia</span></Link>;
+  return <Link href="/" className="brand" aria-label="Ir al inicio de TUCITA"><span className="logo"><Route size={19}/></span><span>TUCITA</span></Link>;
 }
