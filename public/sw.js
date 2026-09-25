08383af426d991d1461f524e712589d2035f7d5b
jorluisproyect <jorluisproyect@gmail.com>
2026-09-25 14:34:20 -0400

    Force installed TUCITA to refresh to latest production shell

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='tucita-shell-v4';
+const CACHE='tucita-shell-v5';
 const SHELL=['/manifest.json','/icons/icon-192.png','/icons/icon-512.png'];
 
 self.addEventListener('install',event=>{
